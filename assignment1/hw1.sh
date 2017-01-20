d1653e959f84b5d49aa4d2d2aea5748971402388
Jon <jonathanwheeler8@gmail.com>
2017-01-20 05:36:57 +0000

    Work on assignment 1. Ready to turn in.

--- assignment1/hw1.sh
+++ assignment1/hw1.sh
@@ -5,7 +5,7 @@
 # 
 #         USAGE: ./hw1.sh 
 # 
-#   DESCRIPTION: 
+#   DESCRIPTION: Homework number 1
 # 
 #         NOTES: ---
 #        AUTHOR: Jon Wheeler (), jonathanwheeler1@weber.edu
@@ -14,5 +14,82 @@
 #===============================================================================
 
 #set -o nounset                              # Treat unset variables as an error
+
+help()
+{
+	echo "Usage $0 [--help] [-w]"
+	echo "--help Print this help message"
+	echo "-w Print name three times"
+	echo "With no arguments it provides a menu to test the system"
+}
+
+userCheck()
+{
+	case "$UID" in
+	0)
+		echo "Welcome great and almighty root"
+		;;
+	*)
+		echo "Current user: $UID"
+		;;
+esac
+}
+
+osCheck()
+{
+	os=`uname -s`
+	if [[ $os == "Linux" ]]
+	then
+		echo "Current operating system: $os"
+	else
+		echo "We are not on Linux but, $os"
+	fi
+}
+
+if [[ $1 == "--help" ]]
+then
+	help
+	exit 0
+fi
+
+echo "My name is Jonathan Wheeler"
+echo "You are running this script in  $HOSTNAME"
+
+echo "This script can do three things:"
+echo "1. Check to see if the user is the root user"
+echo "2. Check to see if the script is running on Linux OS"
+echo "3. Check to see if the -w argument was given"
+echo "What would you like to do? (1, 2, 3): "
+
+read answer
+
+if [[ $answer == 1 ]]
+then
+	userCheck
+fi
+
+if [[ $answer == 2 ]]
+then
+	osCheck
+fi
+
+if [[ $answer == 3 ]]
+then
+	if [[ $1 == "-w" ]]
+	then
+		if [[ $# == 2 ]]
+		then
+			echo "$2 $2 $2"
+			exit 0
+		else
+			echo "-w requires a 2nd parameter"
+			exit 1
+		fi
+	else
+		echo "requires param -w and 2nd param to be repeated. "
+	fi
+fi
+
+
 exit 0
 
