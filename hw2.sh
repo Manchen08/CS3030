eeba517ebd8b56076045d377b90bed73e0374ee1
Jon <jonathanwheeler8@gmail.com>
2017-01-25 23:50:25 +0000

    Assignment2 start

---
+++ hw2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: hw2.sh
+# 
+#         USAGE: ./hw2.sh 
+# 
+#   DESCRIPTION: 
+# 
+#         NOTES: ---
+#        AUTHOR: Jon Wheeler (), jonathanwheeler1@weber.edu
+#       CREATED: 01/25/2017 23:49
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+exit 0
+
