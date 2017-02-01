a21107959fccf0294b68788b3b438196b34c812e
Jon <jonathanwheeler8@gmail.com>
2017-02-01 05:32:41 +0000

    Added hw3

---
+++ assignment3/hw3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: hw3.sh
+# 
+#         USAGE: ./hw3.sh 
+# 
+#   DESCRIPTION: 
+# 
+#         NOTES: ---
+#        AUTHOR: Jon Wheeler (), jonathanwheeler1@weber.edu
+#       CREATED: 02/01/2017 05:31
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+exit 0
+
