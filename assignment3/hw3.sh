8829e5807a4928b5e0bb29cd6df421cc6604f094
Jon Wheeler <jonathanwheeler1@weber.edu>
2017-02-02 11:45:26 -0700

    HW3 recover

--- assignment3/hw3.sh
+++ assignment3/hw3.sh
@@ -14,5 +14,49 @@
 #===============================================================================
 
 #set -o nounset                              # Treat unset variables as an error
+dataFolder="fredData"
+dataFile="FRED.csv"
+
+help()
+{
+	echo "Usage $0 [-c customerDataFolder] [-f dataFile]"
+	echo "Both arguments are required"
+}
+
+if [[ $# != 4 ]]
+then
+	help
+	exit 1
+fi
+
+if [[ $1 == "--help" ]]
+then
+	help
+	exit 0
+fi
+
+if [[ $1 == "-c" ]]
+then
+	$dataFolder=$2
+fi
+
+if [[ ! -d $dataFolder ]]
+then
+	echo "Customer $dataFolder is missing"
+	echo "Creating folder"
+	mkdir -p $dataFolder/{01..12}
+fi
+
+
+if [[ $3 == "-f" ]]
+then
+	$dataFile=$4
+fi
+
+echo "Getting file from customer server"
+
+scp jwheeler@icarus.cs.weber.edu:/home/submit/cs3030/files/$dataFile 
+
 exit 0
+
 
